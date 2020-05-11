a800e72446e6b474433a3bd8992bbb96e31cd162
Tolu Alimi <tolualimi05@gmail.com>
2020-05-11 10:27:19 -0500

    Created timeblock function

--- Work-Day-Scheduler/assets/script.js
+++ Work-Day-Scheduler/assets/script.js
@@ -1,4 +1,4 @@
-/* Current Day and Time */
+/* Current Day Display */
 
 var today = new Date();
 var monthList = [
@@ -22,6 +22,31 @@ var currYear = today.getFullYear();
 
 var currDate = currMonth + " " + currDay + " " + currYear;
 
+var currHour = today.getHours();
+
 $("#currentDay").text(currDate);
+
+/* Timeblock Creation and Formatting */
+
+
+for (var i = 0; i < 24; i++){
+    createTimeBlock(i);
+}
+
+function createTimeBlock(index) {
+    var timeblock = $("<div>").addClass("col-md-10 jumbotron").attr("data-time", index);
+    if (index > currHour) {
+        timeblock.addClass("future");
+    }
+    else if (index === currHour) {
+        timeblock.addClass("present");
+    }
+    else {
+        timeblock.addClass("past");
+    }
+    var inputText = $("<textarea>");
+    timeblock.append(inputText);
+    return timeblock;
+}
 
 
