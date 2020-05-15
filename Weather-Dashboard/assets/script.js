48f5adbe313ff7196b0cc8f9d90dfd7c795c0a68
Tolu Alimi <tolualimi05@gmail.com>
2020-05-15 11:29:56 -0500

    created initialize function to format app before any user input. Started setCurrentWeather function to use the city name to print current weather data

--- Weather-Dashboard/assets/script.js
+++ Weather-Dashboard/assets/script.js
@@ -12,7 +12,14 @@ var cityList = [
     "Seattle"
 ]
 
+var weatherTodayEl = $("#data-today");
+
 var queryURL;
+
+var today = new Date();
+var currDay = today.getDate();
+var currMonth = today.getMonth() + 1;
+var currYear = today.getFullYear();
 
 initialize();
 
@@ -29,13 +36,36 @@ function initialize() {
 }
 
 function setCurrentWeather() {
+    weatherTodayEl.empty();
+    var date = "(" + formatDate(currDay, currMonth, currYear) + ")";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
 
+        cityName = response.name;
+        var temperature = kelvinToFahrenheit(response.main.temp).toFixed(0);
+        var humidity = response.main.humidity;
+        var windSpeed = response.wind.speed;
+        var uvIndex;
         console.log(response);
-        // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
+
+        var headEl = $("<h1>").html(cityName + " " + date);
+        
+        var tempEl = $("<p>").text("Temperature: " + temperature + " F");
+        var humidityEl = $("<p>").text("Humidity: " + humidity + "%");
+        var windSpeedEl = $("<p>").text("Wind Speed: " + windSpeed + " mph");
+        
+        weatherTodayEl.append(headEl, tempEl, humidityEl, windSpeedEl);
 
     });
+}
+
+function kelvinToFahrenheit(tempK) {
+    tempF = (tempK - 273.15) * 1.80 + 32;
+    return tempF;
+}
+
+function formatDate(day, month, year) {
+    return month + "/" + day + "/" + year;
 }
